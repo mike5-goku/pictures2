c1995621d93445227c42c12501e16f5ca9352500
mike5-goku <dylanmacias173@gmail.com>
2025-05-16 13:16:13 -0700

    nuevorepositorio

---
+++ src/Api.js
@@ -0,0 +1,23 @@
+ import axios from "axios";
+
+const searchImage = async(term) => {
+    //const url = 'https://api.unsplash.com/search/photos/?client_id=45kxOXjX5YFWzr5PArB9hUUP9gxNBdxvOEgJsAfnRqE'
+      const url = 'https://api.unsplash.com/search/photos/?client_id=45kxOXjX5YFWzr5PArB9hUUP9gxNBdxvOEgJsAfnRqE'
+
+    const response = await axios.get(url, {
+        params:{
+            query: term
+        }
+    })
+
+    console.log(response)
+    return response.data.results
+}
+
+export default searchImage
+
+
+
+
+
+
